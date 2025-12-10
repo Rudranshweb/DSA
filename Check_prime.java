ccee4d0ce26279ebffd976bf84273ec50fe57c02
Rudranshweb <rudranshsirohia@gmail.com>
2025-12-10 22:37:32 +0530

    Add files via upload

---
+++ Check_prime.java
@@ -0,0 +1,18 @@
+public class Check_prime {
+      public static boolean isprime(int n){
+    if(n==2){
+      return true;
+    }
+    for(int i=2; i<=Math.sqrt(n);i++){
+      if(n%i==0){
+        return false;
+      }
+    }
+      return true;
+    }
+    public static void main(String[] args) {
+        int n=5;
+        System.out.print(isprime(n));
+    }
+    
+}
